96714f2e79e1162ad60d17dce374a768c78b0407
KarsaiKoppany2 <karsai.koppany.2023d@telekimezotur.hu>
2026-09-25 10:43:37 +0200

    2. feladat

---
+++ 02_AdatvedelemEsKonstruktorok/Program.cs
@@ -0,0 +1,10 @@
+﻿using _02_AdatvedelemEsKonstruktorok;
+
+Bankszamla Anna = new Bankszamla("100");
+Bankszamla Bela = new Bankszamla("200", 100000, 50000);
+
+Anna.Befizetes(100000);
+Bela.Kivetel(20000);
+
+Console.WriteLine($"Anna számláján {Anna.Egyenleg} Ft és {Anna.HitelKeret} Ft hitelkeret van.");
+Console.WriteLine($"Béla számláján {Bela.Egyenleg} Ft és {Bela.HitelKeret} Ft hitelkeret van.");
